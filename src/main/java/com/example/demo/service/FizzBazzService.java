d50673f798f8a2e66a2131deac709415a719566c
unknown <takumihikaru39@gmail.com>
2024-09-16 23:06:37 +0900

    コメントを追加

--- src/main/java/com/example/demo/service/FizzBazzService.java
+++ src/main/java/com/example/demo/service/FizzBazzService.java
@@ -18,9 +18,10 @@ public class FizzBazzService {
 	FizzBazzForm fizzBazzForm;
 
 	public List<String> fizzBazzResult(int fizz) {
-
+		//Controllerから取得したfizzに格納されている値に+100した値をnumberに代入する
 		int number = fizz + 100;
 		List<String> fizzBazzDetail = new ArrayList<String>();
+		//fizzBazzの値をfizzBazzDetailにかくのうしている
 		for (fizz = 1; fizz <= number; fizz++) {
 			if (fizz % 15 == 0) {
 				fizzBazzDetail.add("fizzBazz");
@@ -39,9 +40,10 @@ public class FizzBazzService {
 	}
 
 	public List<Integer> intvalue(int fizz) {
-
+		//Controllerから取得したfizzに格納されている値に+100した値をnumberに代入する
 		int number = fizz + 100;
 		List<Integer> getNumber = new ArrayList<Integer>();
+		//numberに格納されている値までの値をgetNumberに格納している
 		for (fizz = 1; fizz <= number; fizz++) {
 			if (fizz % 15 == 0) {
 				getNumber.add(fizz);
@@ -59,6 +61,7 @@ public class FizzBazzService {
 		return getNumber;
 	}
 
+	//過去3件のデータを引っ張ってきている
 	public List<Integer> getPastData() {
 		return fizzBazzMapper.pastData();
 	}
